614124615f48c930a8e571b479a5bef663fdc4fe
Nolan Lawson <nolan.lawson@gmail.com>
2015-05-03 17:37:09 -0400

    add comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// TODO: this should probably be in the "extras" API
 var pluginBase = require('pouchdb/lib/plugins/base');
 var adapterConfig = require('./config');
 var downAdapter = require('asyncstorage-down');
